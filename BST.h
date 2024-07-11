f83eeba159b26733c278cd6fc1dc5c394f3b828e
bapuliz <87442802+bapuliz@users.noreply.github.com>
2024-07-11 16:14:05 -0700

    feat: add new functions to BST.h

--- BST.h
+++ BST.h
@@ -1,23 +1,41 @@
 #include "BSTNode.h"
+#include <iostream>
 #ifndef BST_H
 #define BST_H
 
 class BST {
     BSTNode* root;
     public:
-        BST() : root(nullptr) {}
-        BSTNode* insert(BSTNode* node, const int& value) {
-            if (!node){
-                BSTNode* node_to_insert = new BSTNode(value);
-                return node_to_insert;
-            }
-            if (value > node->value) {
-                node->right = insert(node->right, value);
-            } else {
-                node->left = insert(node->left, value);
-            }
-            return node;
+    BST() : root(nullptr) {}
+    BSTNode* insert(BSTNode* node, const int& value) {
+        if (!node){
+            BSTNode* node_to_insert = new BSTNode(value);
+            return node_to_insert;
         }
+        if (value > node->value) {
+            node->right = insert(node->right, value);
+        } else {
+            node->left = insert(node->left, value);
+        }
+        return node;
+    }
+    void insert(const int& value) {
+        root = insert(root, value);
+    }
+    void print(const BSTNode* const node) const {
+        if (!node) {
+            return;
+        }
+        
+        std::cout << node->value << std::endl;
+        print(node->left);
+        print(node->right);
+        
+    }
+    void print() {
+        print(root);
+    }
+
 };
 
 
--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 
 int main() {
     BST t;
-
+    t.insert(1);
+    t.insert(2);
+    t.insert(3);
+    t.insert(4);
+    t.print();
     return 0;
 }
