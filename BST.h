c523b2728e1e899ca3b22726f155fa4805f1f948
bapuliz <87442802+bapuliz@users.noreply.github.com>
2024-07-13 15:34:42 -0700

    feat: add DFS and BFS to BST.h

--- BST.h
+++ BST.h
@@ -1,5 +1,6 @@
 #include "BSTNode.h"
 #include <iostream>
+#include <queue>
 #ifndef BST_H
 #define BST_H
 
@@ -19,21 +20,96 @@ class BST {
         }
         return node;
     }
+    void insertIterative(const int& value) {
+        BSTNode* node_to_insert = new BSTNode(value);
+        if (root == nullptr) {
+            root = node_to_insert;
+            return;
+        }
+        BSTNode* current = root;
+        while (current) {
+            if (value < current->value) {
+                if (current->left == nullptr) {
+                    current->left = node_to_insert;
+                    return;
+                }
+                current = current->left;
+            } else {
+                if (current->right == nullptr) {
+                    current->right = node_to_insert;
+                    return;
+                }
+                current = current->right;
+            }
+        }
+
+    }
     void insert(const int& value) {
         root = insert(root, value);
+    }
+
+    BSTNode* remove(BSTNode* node, const int& value) {
+        if (!node) {
+            return node;
+        }
+        if (value < node->value) {
+            node->left = remove(node->left, value);
+        } else if (value > node->value) {
+            node->right = remove(node->right, value);
+        } else {
+            if (node->left == nullptr) {
+                BSTNode* t = node->right;
+                delete node;
+                return t;
+            } else if (node->right == nullptr) {
+                BSTNode* t = node->left;
+                delete node;
+                return t;
+            }
+            BSTNode* min_node = findMin(node->right);
+            node->value = min_node->value;
+            node->right = remove(node->right, min_node->value);
+        }
+        return node;
+    }
+    void remove(const int& value) {
+        root = remove(root, value);
+    }
+
+    BSTNode* findMin(BSTNode* const node) const {
+        BSTNode* current = node;
+        while (current != nullptr && current->left != nullptr) {
+            current = current->left;
+        }
+        return current;
     }
     void print(const BSTNode* const node) const {
         if (!node) {
             return;
         }
         
+        print(node->right);
         std::cout << node->value << std::endl;
         print(node->left);
-        print(node->right);
         
     }
+    void printBST() const {
+        std::queue<BSTNode*> q;
+        q.push(root);
+        while (!q.empty()){
+            BSTNode* current = q.front();
+            if (current->left) {
+                q.push(current->left);
+            }
+            if (current->right) {
+                q.push(current->right);
+            }
+            std::cout << current->value << std::endl;
+            q.pop();
+        }
+    }
     void print() {
-        print(root);
+        printBST();
     }
 
 };
--- main.cpp
+++ main.cpp
@@ -3,10 +3,12 @@
 
 int main() {
     BST t;
+    t.insertIterative(4);
     t.insert(1);
     t.insert(2);
     t.insert(3);
-    t.insert(4);
+    t.insert(5);
     t.print();
+
     return 0;
 }
